3cf1071cc2277bfbd6f0f6b7bf1779ebb5b669fa
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-03-03 08:57:33 +0800

    contact-me - added contact form using short codes

--- wp-content/themes/angedove-theme/page-contact.php
+++ wp-content/themes/angedove-theme/page-contact.php
@@ -13,7 +13,11 @@ get_header();
 <div class="row">
     <div class="content-entry">
          <div class="medium-7 large-7 columns">
-             <?php get_template_part("partials/form", "contact"); ?>
+             <?php// get_template_part("partials/form", "contact"); ?>
+
+              <?php echo do_shortcode('[contact-form-7 id="82" title="contact-me"]'); ?> 
+
+              <br><br>
          </div>
         <div class="medium-5 large-4 columns">
             <?php  get_sidebar(); ?>
